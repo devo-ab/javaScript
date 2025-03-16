4739c17fd4da84a957ada75277c3e2f75befdef6
devo-ab <per.useravi0@gmail.com>
2025-03-16 11:39:09 +0600

    second capter complete

---
+++ index.js
@@ -0,0 +1,8 @@
+const weight = 38;
+const price = 760;
+const year = 1999;
+const balance = 254;
+// const age = 45;
+
+const age = [11, 15679, 1, 489, 68914, "string"];
+console.log(age);
---
+++ secondCapter.js
@@ -0,0 +1,33 @@
+// Details Of Mine
+const myBirthDate = 1999;
+const mySister = 2;
+const students = 46;
+const examMarks = 70;
+const rooms = 3;
+const studyHours = 6;
+const sumOfPhone = 30;
+
+// practice
+const myAge = 24;
+console.log(`I am ${myAge} years Old.`);
+
+const booksToRead = 10;
+console.log(`I want read ${booksToRead} books this year.`);
+
+const wakeUpTime = 6;
+const updateWakeUpTime = 5;
+console.log(
+  `Generally wake up at ${wakeUpTime} but after today i will wake at ${updateWakeUpTime}`
+);
+
+const wastedTime = 5;
+console.log(`Today i wasted time in mobile phone $${wastedTime} hours`);
+
+const borrow = 2000;
+console.log(`Borrow ${borrow} BDT from my elder brother`);
+
+const goals = 12;
+console.log(`Argentina scored ${goals} goals against Ecuador`);
+
+const pillow = 3;
+console.log(`I need ${pillow} pillow to sleep`);
