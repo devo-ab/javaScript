7dc124a0e28e8f4bdd1a095e1783e2b0ce3d66cc
devo-ab <per.useravi0@gmail.com>
2025-03-17 10:15:04 +0600

    second capter complete fully

--- secondCapter.js
+++ secondCapter.js
@@ -31,3 +31,117 @@ console.log(`Argentina scored ${goals} goals against Ecuador`);
 
 const pillow = 3;
 console.log(`I need ${pillow} pillow to sleep`);
+
+// string
+const name = "Avijit Biswas";
+console.log(name);
+
+const address = "Mirpur-1, Dhaka, Bangladesh";
+console.log(address);
+
+const frd = "Tarail Secondary School";
+console.log(frd);
+
+const fanOf = "Pori Moni";
+console.log(fanOf);
+
+const movieOfTheYear = "Ponman";
+console.log(movieOfTheYear);
+
+const hobby = "Playing football";
+console.log(hobby);
+
+const color = "Black";
+console.log(color);
+// string
+
+// boolean
+const frdIsHappy = false;
+console.log(frdIsHappy);
+
+const crushedReplied = false;
+console.log(crushedReplied);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(
+  `I am very happy(${isHappy}), because i have lot of money (${hasMoney}), I am hungry now(${isHungry})`
+);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
+// boolean
+
+// math
+const eggPrice = 10;
+const onionPrice = 20;
+const totalPrice = eggPrice + onionPrice;
+const diffPrice = onionPrice - eggPrice;
+console.log(totalPrice);
+console.log(diffPrice);
+
+// reminder
+console.log(11 / 7); // division
+console.log(11 % 7); // reminder
+// reminder
+
+const firstName = "Avijit";
+const lastName = "Biswas";
+const fullName = firstName + lastName;
+console.log(fullName);
+
+const sugar = 20;
+const daddy = "20";
+console.log(sugar + daddy);
+
+const earn = 100
+const buyFruit = 40
+console.log("After Buying Fruit I Have",earn - buyFruit, "BDT")
+
+const pencil = 10
+const useAtTime = 2
+console.log("I can use 10 pencil", pencil / useAtTime, "times")
+
+const money = 60
+const burn = 15
+console.log(`I can use 60 BDT for ${money / burn} times and after using ${money / burn} times, i have extra ${money % burn} BDT`)
+
+const first = "Hello"
+const last = "World"
+console.log(first + last)
+
+const cal = 153
+const dal = 261
+console.log(`calDal ${cal + dal} KG`)
+
+const iHave = 500
+const iUsed = 75
+console.log(`I can use 500 BDT  ${iHave / iUsed} times and after used ${iHave / iUsed} times, i have extra ${500 % 75} BDT`)
+
+const big = 8
+const small = 3
+console.log(`Division ${8 / 3} , reminder ${8 % 3}`)
+
+const Num1 = 50
+const Num2 = 9
+console.log(`Reminder ${50 % 9}`)
+
+const fiName = "Bangla"
+const laName = "desh"
+console.log(fiName + laName)
+
+const haveRich = 480
+const eatRich = 4
+const month = 30
+console.log(`I can survive ${haveRich / eatRich} days, and ${(haveRich / eatRich) / month} month`)
+// math
